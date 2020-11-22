58f6e6144992c1784ac96ead8b7888466ca92e5e
rodrigo.hernandezdev <rodrigo.hernandezdev@gmail.com>
2020-11-22 17:14:16 -0600

    First commit

---
+++ src/main/java/com/example/movierent/MovieRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.movierent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movierent/MovieRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
