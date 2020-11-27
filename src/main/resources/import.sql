bdd4f16c3521270b2ad8b985b1e4149f4e62b737
rodrigo.hernandezdev <rodrigo.hernandezdev@gmail.com>
2020-11-26 21:50:32 -0600

    Config for work in heroku

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,9 +6,3 @@ INSERT INTO roles(role_id, description, name) VALUES (2, 'User role', 'USER');
 
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
-
-INSERT INTO movies(movie_id, tittle, description, stock, availability, likes, rental_price, sale_price ) VALUES (1, 'Movie1', 'description1', 100, true, 0, 20, 100);
-INSERT INTO movies(movie_id, tittle, description, stock, availability, likes, rental_price, sale_price ) VALUES (2, 'Movie2', 'description2', 100, true, 0, 20, 100);
-INSERT INTO movies(movie_id, tittle, description, stock, availability, likes, rental_price, sale_price ) VALUES (3, 'Movie3', 'description3', 100, true, 0, 20, 100);
-INSERT INTO movies(movie_id, tittle, description, stock, availability, likes , rental_price, sale_price) VALUES (4, 'Movie4', 'description4', 100, true, 0, 20, 100);
-INSERT INTO movies(movie_id, tittle, description, stock, availability, likes , rental_price, sale_price) VALUES (5, 'Movie5', 'description5', 100, true, 0, 20, 100);
